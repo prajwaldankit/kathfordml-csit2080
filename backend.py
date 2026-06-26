5aaecca6c66ed07735c4e2c03a94d2081dd146cf
prajwaldankit <prajwal.x82@gmail.com>
2026-06-26 08:48:55 +0545

    Basic python code snippets

---
+++ backend.py
@@ -0,0 +1,15 @@
+from utils import *
+import datetime
+
+
+def app():
+    print(add(1, 2))
+    print(add2(2, 2))
+    print(add3(2, 2))
+    print("this is the main application.")
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    app()
---
+++ utils.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    print(__name__)
+    return a + b
+
+
+def add2(a, b):
+    return a + b
+
+
+def add3(a, b):
+    return a + b
+
+
+def add4(a, b):
+    return a + b
+
+
+def add5(a, b):
+    return a + b
