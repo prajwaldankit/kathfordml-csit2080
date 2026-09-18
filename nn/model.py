feb6dabfec039cc274033ce1f5f0cf218e028422
prajwaldankit <prajwal.x82@gmail.com>
2026-09-18 10:01:19 +0545

    refactor: enhance neural network architecture and training loop

--- nn/model.py
+++ nn/model.py
@@ -8,12 +8,15 @@ class NeuralNetwork(nn.Module):
 
 
         self.network = nn.Sequential(
-            nn.Linear(784,128),
+            nn.Linear(784,256),
             nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256,128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(128,64),
             nn.ReLU(),
-            nn.Linear(64,10) 
-
+            nn.Linear(64,10),
         )
 
     def forward(self, x):
--- nn/train.py
+++ nn/train.py
@@ -2,6 +2,7 @@ import random
 
 import torch
 from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
 from matplotlib import pyplot as plt
 
 from model import NeuralNetwork
@@ -29,27 +30,37 @@ optimizer = torch.optim.Adam(
     lr=0.001
 )
 
+train_loader = DataLoader(
+    TensorDataset(X_train, y_train),
+    batch_size=64,
+    shuffle=True,
+)
+
 epochs = 20
 losses = []
 
 for _ in range(epochs):
     model.train()
 
-    loss = 0
+    epoch_loss = 0
 
-    predictions = model(X_train)
+    for batch_X, batch_y in train_loader:
+        predictions = model(batch_X)
 
-    loss = criterion(predictions, y_train)
+        loss = criterion(predictions, batch_y)
 
-    optimizer.zero_grad()
+        optimizer.zero_grad()
 
-    loss.backward()
+        loss.backward()
 
-    optimizer.step()
+        optimizer.step()
 
-    losses.append(loss.item())
+        epoch_loss += loss.item()
+
+    losses.append(epoch_loss / len(train_loader))
 
 
+model.eval()
 with torch.no_grad():
     test_outputs = model(X_test)
     test_predictions = torch.argmax(test_outputs, dim=1)
@@ -61,10 +72,6 @@ with torch.no_grad():
     test_accuracy = (
         test_correct/ y_test.shape[0]
     )
-    print(f"test_accuracy = {test_accuracy * 100}")
-
-
-
 
     img = X_test[test_index].reshape(28,28)
     plt.imshow(img, cmap="gray")
@@ -72,4 +79,4 @@ with torch.no_grad():
     plt.axis("off")
     plt.show()
     
-
+    print(f"test_accuracy = {test_accuracy * 100}")
